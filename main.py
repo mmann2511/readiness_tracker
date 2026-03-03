d1af6ed8f764347890f55d1fdd9c66226c7bd684
mmann2511 <michaelgmann25@gmail.com>
2026-03-03 16:05:09 -0700

    Refactor main_menu and related functions to use unit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from data_manager import load_data
+from unit import Unit
 
-
-def main_menu():
+def main_menu(unit):
 
     menu = """
     MAIN MENU
@@ -15,27 +15,31 @@ def main_menu():
         print(menu)
         choice = input("Input: ")
         if choice == "1":
-            unit_commands()
+            unit_commands(unit)
         elif choice == "2":
-            flight_commands()
+            flight_commands(unit)
         elif choice == "3":
-            operator_commands()
+            operator_commands(unit)
         else:
             break
     
-def unit_commands():
+def unit_commands(unit):
     menu = """
-    Unit commands coming soon
-    1. Main Menu
+    Unit commands:
+    1. Unit Average Test Score
+    2. Unit Readiness
+    3. Flight Information
+    4. Add Flight
+    5. Main Menu
     """   
 
     while True:
         print(menu)
         choice = input("Input: ")
         if choice == "1":
-            return   
+               
 
-def flight_commands():
+def flight_commands(unit):
     menu = """
     Flight commands coming soon
     1. Main Menu
@@ -47,7 +51,7 @@ def flight_commands():
         if choice == "1":
             return
 
-def operator_commands():
+def operator_commands(unit):
     menu = """
     Operator commands coming soon
     1. Main Menu
@@ -60,10 +64,12 @@ def operator_commands():
             return
 
 def main():
-    data = load_data()
-
-    if data is None:
-        main_menu()
+    unit = load_data()
+    if unit is None:
+        name = input("What is the unit name? ")
+        unit = Unit(name)
+    
+    main_menu(unit)
 
 
 
